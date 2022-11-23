6406f533e9ba456b6f6ad41920ad568192e22296
Dhruvidave1 <dhruvi@ualberta.ca>
2022-11-23 16:20:35 -0700

    modified sql files

--- ensf608_queries.sql
+++ ensf608_queries.sql
@@ -1,3 +1,25 @@
-SELECT *
-FROM
-COURSE
+USE ASSIGNMENT6;
+
+#	Show all tables, explain how they are related to one another (keys, triggers, etc.)
+SELECT * FROM COURSE;
+SELECT * FROM STUDENT;
+SELECT * FROM PREREQUISITES;
+SELECT * FROM DEPARTMENT;
+SELECT * FROM ENROLS;
+SELECT * FROM OFFERINGS;
+SELECT * FROM ADMIN;
+
+#	A basic retrieval query: Show all courses each student is enrolled insert
+
+
+
+#	A retrieval query with ordered results
+
+#	A nested retrieval query
+
+#	A retrieval query using joined tables
+
+#	An update operation with any necessary triggers
+
+#	A deletion operation with any necessary triggers
+ 

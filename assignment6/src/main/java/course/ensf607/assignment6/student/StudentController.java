cc75fbc9ead516c7f24ddfa61534623aa89a8c21
Dhruvidave1 <dhruvi@ualberta.ca>
2022-11-25 16:04:11 -0700

    cross origin

--- assignment6/src/main/java/course/ensf607/assignment6/student/StudentController.java
+++ assignment6/src/main/java/course/ensf607/assignment6/student/StudentController.java
@@ -8,7 +8,7 @@ import course.ensf607.assignment6.course.Course;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
+@CrossOrigin
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {

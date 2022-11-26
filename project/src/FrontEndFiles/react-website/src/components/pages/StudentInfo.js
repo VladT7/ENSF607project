8330d4480528d12cec12ed0eaf7695dbd9dc6dca
Dhruvidave1 <dhruvi@ualberta.ca>
2022-11-26 15:26:55 -0700

    added student functions

--- project/src/FrontEndFiles/react-website/src/components/pages/StudentInfo.js
+++ project/src/FrontEndFiles/react-website/src/components/pages/StudentInfo.js
@@ -1,5 +1,7 @@
 import '../../App.css';
 import React, { useState} from 'react';
+import EnrollCourseForm from '../EnrollCourseForm';
+import DropCourseForm from '../DropCourseForm';
 
 function StudentInfo(){
 
@@ -21,25 +23,6 @@ function StudentInfo(){
         console.log(url);
         console.log(result);
         setuserInput('');        
-    }
-
-    const [name, setname] = useState('');
-    const [ucid, setucid] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const enroll = {name, ucid};
-        const url = 'http://localhost:8080/api/v1/course/'+name+'/students/'+ucid;
-        fetch(url,{
-            method:'PUT',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(enroll)
-        }). then(() => {
-            console.log('i just did something');
-            setname('');            
-            setucid('');
-        })
-        
     }
 
     
@@ -64,27 +47,13 @@ function StudentInfo(){
 
         <hr></hr>
 
-        <h1>To enroll in a course, please enter the course name and your ucid below:</h1>
-        <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label>Course Name</label>
-                <input type="text" value = {name} onChange={(e) => setname(e.target.value)} className="form-control" placeholder="Enter Course Name, e.g.: ENSF590"></input>
+        <EnrollCourseForm/>
         
-            </div>
+        <hr></hr>
 
-            <div className="form-group">
-                <label>UCID:</label>
-                <input type="text" value = {ucid} onChange={(e) => setucid(e.target.value)} className="form-control" placeholder="Enter your UCID, e.g 2800232"></input>
-            </div>
+        <DropCourseForm/>
 
-            <button type="submit" className="btn btn-primary btn-lg">Enroll</button>
-        </form>     
-
-
-
-
-        </div>        
-        
+        </div>            
 
          
         </>

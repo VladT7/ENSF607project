e81910999881aff770cdd3aaf93b5883a2892e5d
coryashannon <coryashannon@gmail.com>
2022-11-29 19:46:28 -0700

    .sql files for first presentation

--- Queries.sql
+++ Queries.sql
@@ -1,55 +1,53 @@
 USE ASSIGNMENT6;
 
 #	Show all tables, explain how they are related to one another (keys, triggers, etc.)
--- SELECT * FROM COURSE;
--- SELECT * FROM STUDENT;
--- SELECT * FROM PREREQUISITES;
--- SELECT * FROM DEPARTMENT;
--- SELECT * FROM ENROLS;
--- SELECT * FROM OFFERINGS;
--- SELECT * FROM ADMIN;
+SELECT * FROM COURSE;
+SELECT * FROM STUDENT;
+SELECT * FROM PREREQUISITES;
+SELECT * FROM DEPARTMENT;
+SELECT * FROM ENROLS;
+SELECT * FROM OFFERINGS;
+SELECT * FROM ADMIN;
 
 -- #(2)	A basic retrieval query: [Course Name, OfferingID, StudentID]
 -- # A master table showing enrollment and course details for students in the MEng cohort with details on offering and course ID(Result 8)
--- SELECT 
---     e.OfferingID, e.StudentID, e.Grade, c.courseName
--- FROM
---     ENROLS AS e,
---     COURSE AS c
---         INNER JOIN
---     OFFERINGS AS o
--- WHERE
---     e.OfferingID = o.OfferingID
---         AND c.courseID = o.CourseID;
+SELECT 
+    e.OfferingID, e.StudentID, e.Grade, c.courseName
+FROM
+    ENROLS AS e,
+    COURSE AS c
+        INNER JOIN
+    OFFERINGS AS o
+WHERE
+    e.OfferingID = o.OfferingID
+        AND c.courseID = o.CourseID;
 
 -- -- #(3)	A retrieval query with ordered results
 -- # A query showing the offering listed in increasing order by year offered
--- SELECT o.Year_offered, o.Semester, c.courseName
--- from
--- 	OFFERINGS AS o
---     INNER JOIN
---     COURSE AS c
--- WHERE
--- 	o.courseID = c.courseID
--- ORDER BY
--- 	o.Year_offered, o.Semester;
+SELECT o.Year_offered, o.Semester, c.courseName
+from
+	OFFERINGS AS o
+    INNER JOIN
+    COURSE AS c
+WHERE
+	o.courseID = c.courseID
+ORDER BY
+	o.Year_offered, o.Semester;
 
 -- -- #	A nested retrieval query [Student Full Name, Grade, CourseName] for student with highest and lowest grades across all courses
--- SELECT 
---     s.FName, s.LName, e.Grade, c.courseName, o.Year_offered
--- FROM STUDENT AS s    
---     INNER JOIN ENROLS AS e ON e.StudentID = s.StudentID     
---     INNER JOIN OFFERINGS AS o ON e.OfferingID = o.OfferingID    
---     INNER JOIN COURSE AS c ON o.courseID = c.courseID        
--- WHERE
---     Grade = (SELECT MAX(Grade) FROM ENROLS) OR Grade = (SELECT MIN(Grade) FROM ENROLS);
+SELECT 
+    s.FName, s.LName, e.Grade, c.courseName, o.Year_offered
+FROM STUDENT AS s    
+    INNER JOIN ENROLS AS e ON e.StudentID = s.StudentID     
+    INNER JOIN OFFERINGS AS o ON e.OfferingID = o.OfferingID    
+    INNER JOIN COURSE AS c ON o.courseID = c.courseID        
+WHERE
+    Grade = (SELECT MAX(Grade) FROM ENROLS) OR Grade = (SELECT MIN(Grade) FROM ENROLS);
 
--- #	A retrieval query using joined tables [CourseName, PreReqs]
--- SELECT c.courseCode, p.PrereqID1, p.PrereqID2, p.PrereqID3, p.PrereqID4
--- FROM COURSE AS c
--- INNER JOIN PREREQUISITES as p ON p.CourseID = c.CourseID;
-
-
+#	A retrieval query using joined tables [CourseName, PreReqs]
+SELECT c.courseCode, p.PrereqID1, p.PrereqID2, p.PrereqID3, p.PrereqID4
+FROM COURSE AS c
+INNER JOIN PREREQUISITES as p ON p.CourseID = c.CourseID;
 
 #	An update operation with any necessary triggers
  -- show tables prior to update
@@ -61,7 +59,6 @@ SET OfferingID = 9 WHERE StudentID = 10138517 AND OfferingID = 4;
 -- show tables after update
 SELECT* FROM ENROLS;
 SELECT * FROM OFFERINGS;
-
 
 #	An inseration and deletion operation with any necessary triggers
 -- show tables prior to insertion and deletion
